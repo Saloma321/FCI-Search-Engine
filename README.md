dc008fb02f5de120c55d77b6831d31becfe07460
Saloma321 <salamoka601@gmail.com>
2024-06-08 01:25:47 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# FCI-Search-Engine
+# Question Answering 
+ - T5, or Text-to-Text Transfer Transformer, is a Transformer based architecture that uses a 
+   text-to-text approach. Every task – including translation, question answering, and classification – is cast as feeding the model text as input and training it to generate some target text. This allows for the use of the same model, loss function, hyperparameters, etc. across our diverse set of tasks.
+
+### AI Models
+
+- Text-to-Text Transfer Transformer (T5)
+
+
+![QA](process_image.jpg)
+
+## Technologies
+- FASTAPI
+- sqlite database
+- Natural Language processing
+- Large Language Model
+- text Processing
+
+
+## Framework 
+- FASTAPI
+
+## Usage
+  git clone https://github.com/Shymaa2611/QA_API.git
+  <br>
+  cd QA_API
+  <br>
+  pip install -r requirements.txt
+  <br>
+  uvicorn main:app --reload
+
+
+### Running
+ 
+  - http://127.0.0.1:8000/docs
+
+
+
+### Deploy
+
+  
+
+
+
+
---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from qamodels import QuestionAnswer,Base
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+DATABASE_URL = "sqlite:///./sql_app.db"
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "No result"}
+
+""" def create_question(question: str, answer: str):
+    session = Session()
+    db_question = QuestionAnswer(question=question, answer=answer)
+    session.add(db_question)
+    session.commit()
+    session.refresh(db_question)
+    session.close()
+    return {"status_code": 200, "message": "success"}
+ """
+
+def get_answer(question:str):
+   checkpoint_path = 'model'
+   checkpoint_path_tokenizer='tokenizer'
+   tokenizer = T5Tokenizer.from_pretrained(checkpoint_path)
+   model = T5ForConditionalGeneration.from_pretrained(checkpoint_path_tokenizer)
+   input_text = question
+   input_ids = tokenizer.encode(input_text, return_tensors='pt')
+   output_ids = model.generate(input_ids, max_length=50, num_beams=4, early_stopping=True)
+   output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True) 
+   return output_text
+
+
+
+@app.get("/get_qa/")
+async def get_data(question:str):
+   # answer=get_answer(question)
+    if question:
+        question_data = {"question": question, "answer":"There is No Answer !"}
+        return question_data
+    else:
+        return "There is No Question !"
